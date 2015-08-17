eb13c34700d36ac301f9c3de4301163b68debf96
Lars Magne Ingebrigtsen <larsi@gnus.org>
2015-08-18 00:16:47 +0200

    Implement.

--- watch-directory.el
+++ watch-directory.el
@@ -1,4 +1,4 @@
-;;; watch-directory.el --- watch directories and insert stuff
+;;; watch-directory.el --- watch directories and insert stuff  -*- lexical-binding: t -*-
 ;; Copyright (C) 2015 Lars Magne Ingebrigtsen
 
 ;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
@@ -24,6 +24,37 @@
 ;;; Commentary:
 
 ;;; Code:
+
+(defun watch-directory (directory)
+  "Watch DIRECTORY for new files and insert them in the buffer when they appear."
+  (interactive "DDirectory to watch: ")
+  (let ((files (directory-files directory t))
+	(buffer (current-buffer))
+	timer)
+    (setq timer
+	  (run-at-time
+	   1 10
+	   (lambda ()
+	     (message "Scanning %s for %s" directory buffer)
+	     (if (not (buffer-live-p buffer))
+		 (cancel-timer timer)
+	       (dolist (file (directory-files directory t))
+		 (unless (member file files)
+		   (with-current-buffer buffer
+		     (save-excursion
+		       (goto-char (point-max))
+		       (let ((point (point)))
+			 (insert "<img src=%S>\n\n" file)
+			 (put-image
+			  (create-image
+			   file 'imagemagick nil
+			   :max-width
+			   (truncate
+			    (* 0.7 (- (nth 2 edges) (nth 0 edges))))
+			   :max-height
+			   (truncate
+			    (* 0.5 (- (nth 3 edges) (nth 1 edges)))))
+			  point))))))))))))
 
 (provide 'watch-directory)
 
